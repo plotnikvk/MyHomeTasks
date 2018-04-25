9f0b2d055105f0a8693b2c6acdc0b78dd89e4605
plotnikvk <plotnikov.vova@mail.ru>
2018-04-25 10:38:46 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        float firstNumber;
+        String operation;
+        float secondNumber;
+
+        Scanner in = new Scanner(System.in);
+        
+
+        System.out.println("Введите первое число c плавающей точкой: ");
+
+
+    }
+}
