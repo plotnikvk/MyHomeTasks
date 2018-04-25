307a64cc2e8fd2814120ed46129dbb96f52cdbb6
plotnikvk <plotnikov.vova@mail.ru>
2018-04-25 13:42:54 +0300

    Updates 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,15 +6,40 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        float firstNumber;
+        double firstNumber;
         String operation;
-        float secondNumber;
+        double secondNumber;
+        double result = 0.0;
 
         Scanner in = new Scanner(System.in);
-        
 
         System.out.println("Введите первое число c плавающей точкой: ");
+        firstNumber = in.nextFloat();
+        System.out.println("Введите второе число c плавающей точкой: ");
+        secondNumber = in.nextFloat();
+        System.out.println("Введите знак операции(Возможны: +, -, *, /, %): ");
+        operation = in.next();
 
+
+
+        switch (operation){
+             case "+":
+                 result = firstNumber + secondNumber;
+                 break;
+             case "-":
+                 result = firstNumber - secondNumber;
+                 break;
+             case "*":
+                 result = firstNumber * secondNumber;
+                 break;
+             case"/":
+                 result = (firstNumber/secondNumber);
+             case "%":
+                 result = firstNumber % secondNumber;
+             default:
+         }
+System.out.println(firstNumber/secondNumber);
+         System.out.printf( "Результат операции %s над числами %.4f и %.4f равен: %.4f" ,operation, firstNumber, secondNumber, result);
 
     }
 }
