072b34b84ecb4bbb67dde74b8dd727dcde287198
plotnikvk <plotnikov.vova@mail.ru>
2018-04-25 22:47:00 +0300

    Updates 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        double firstNumber;
+        double firstNumber;//переменная, в которой сохраняется первое число
         String operation;
         double secondNumber;
 
